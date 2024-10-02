a11428fd8d27e972bb448424ccd41388beb4bf9c
arijiiiitttt <arijit123roy098@gmail.com>
2024-10-02 12:53:18 +0530

    header updated

--- src/components/Base/Header.jsx
+++ src/components/Base/Header.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,37 +10,72 @@ const Header = () => {
 
   return (
     <>
-      <nav className='items-center lg:p-6 flex justify-between' id='homePg'>
-        <div className='flex flex-1 justify-start p-0'>
-          <a className='flex flex-row '>
+      <nav className="flex items-center justify-between lg:p-6 p-4" id="homePg">
+        <div className="flex items-center justify-start flex-1">
+          <a className="flex flex-row items-center">
             <img
-              src='./Logos/a.svg'
-              className='h-12'
-              alt='logo'
+              src="./Logos/a.svg"
+              className="h-12"
+              alt="logo"
               onClick={handleImageClick}
-            /><h2 className='flex text-center popBlack text-2xl'>rijit</h2>
+            />
+            <h2 className="text-2xl popBlack">rijit</h2>
           </a>
-         <a className='flex flex-row'>
-          <img src='/Logos/hi.png' className='h-9'></img>
-          <p>Hello</p>
-         </a>
+          <a className="flex flex-row items-center ml-4">
+            <img src="/Logos/hi.png" className="h-9" alt="hi" />
+            <p>Hello</p>
+          </a>
         </div>
 
-        <ul className='flex flex-row lg:gap-7 lg:text-[20px] font-light'>
-          <li><a href='#homePg'>Home</a></li>
-          <li><a href='#aboutPg'>About</a></li>
-          <li><a href='blog'>Blog</a></li>
-          <li><a href='#projectPg'>Projects</a></li>
-          <li>
+        {/* Hamburger Menu Icon */}
+        <div className="lg:hidden">
+          <button onClick={handleImageClick}>
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Navigation Items */}
+        <ul
+          className={`flex-col lg:flex lg:flex-row lg:gap-7 lg:text-[20px] font-light absolute lg:static left-0 w-full lg:w-auto lg:h-auto lg:bg-transparent bg-white transition-transform transform ${
+            isOpen ? "translate-y-16" : "-translate-y-full"
+          } lg:translate-y-0 p-5 lg:p-0 top-0 lg:top-auto lg:justify-end`}
+          style={{ justifyContent: isOpen ? "flex-start" : "flex-end" }}
+        >
+          <li className="my-2 lg:my-0">
+            <a href="#homePg">Home</a>
+          </li>
+          <li className="my-2 lg:my-0">
+            <a href="#aboutPg">About</a>
+          </li>
+          <li className="my-2 lg:my-0">
+            <a href="blog">Blog</a>
+          </li>
+          <li className="my-2 lg:my-0">
+            <a href="#projectPg">Projects</a>
+          </li>
+          <li className="my-2 lg:my-0">
             <a
-              href='#contactPg'
-              className='hover:bg-black hover:text-white bg-[#ff0000] lg:px-5 lg:py-1 rounded-lg text-white popBold font-normal'>
+              href="#contactPg"
+              className="hover:bg-black hover:text-white bg-[#ff0000] lg:px-5 lg:py-1 px-3 py-1 rounded-lg text-white popBold font-normal"
+            >
               Contact Me
             </a>
           </li>
         </ul>
       </nav>
-
     </>
   );
 };
